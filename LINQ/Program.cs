70dc9f20667a1e867da2ad42c57cf69a0c04e69a
Kai Morris <kaij.morris@yahoo.com>
2022-11-07 12:11:39 -0500

    book names

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> bookNames = new List<string>
+            { "black boy", "blueprint for black power", "the isis papers",
+                "afrikan holistic health"};
+
+            IEnumerable<string> longBookNames =
+                bookNames.Where(name => name.Contains("a"));
+
+            foreach (var o in bookNames)
+            {
+                Console.WriteLine(o);
+            }
         }
+
     }
 }
